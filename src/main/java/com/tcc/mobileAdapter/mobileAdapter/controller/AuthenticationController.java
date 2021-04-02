d2e99b4da9a5b226b9f5b74ecaef295ac7f20d6b
gabriel <gabriel.talles00@gmail.com>
2021-04-02 19:10:57 -0300

    apply cross originm

--- src/main/java/com/tcc/mobileAdapter/mobileAdapter/controller/AuthenticationController.java
+++ src/main/java/com/tcc/mobileAdapter/mobileAdapter/controller/AuthenticationController.java
@@ -14,10 +14,12 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Data
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthenticationController implements Authentication {
 
--- src/main/java/com/tcc/mobileAdapter/mobileAdapter/controller/api/Authentication.java
+++ src/main/java/com/tcc/mobileAdapter/mobileAdapter/controller/api/Authentication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/auth")
-@CrossOrigin
 public interface Authentication {
 
     @PostMapping("/login")
