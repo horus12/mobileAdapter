e4d4281098f8f130df5aba27c408c9d03581c2a0
gabriel <gabriel.talles00@gmail.com>
2021-04-02 19:46:43 -0300

    comented internal create user

--- src/main/java/com/tcc/mobileAdapter/mobileAdapter/usecase/InternalAuthenticationUseCase.java
+++ src/main/java/com/tcc/mobileAdapter/mobileAdapter/usecase/InternalAuthenticationUseCase.java
@@ -3,9 +3,7 @@ package com.tcc.mobileAdapter.mobileAdapter.usecase;
 import com.tcc.mobileAdapter.mobileAdapter.controller.domain.request.AuthRequest;
 import com.tcc.mobileAdapter.mobileAdapter.controller.domain.response.AuthResponse;
 import com.tcc.mobileAdapter.mobileAdapter.data.internaluser.InternalUserRepository;
-import com.tcc.mobileAdapter.mobileAdapter.data.user.UserRepository;
 import com.tcc.mobileAdapter.mobileAdapter.domain.InternalUser;
-import com.tcc.mobileAdapter.mobileAdapter.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -20,9 +18,9 @@ public class InternalAuthenticationUseCase {
         Assert.hasText(authRequest.getUserName(), "UserName should not be null or empty");
         Assert.hasText(authRequest.getPassword(), "Password should not be null or empty");
 
-        InternalUser iUser = InternalUser.builder().email(authRequest.getUserName()).password(authRequest.getPassword()).build();
-
-        internalUserRepository.save(iUser);
+//        InternalUser iUser = InternalUser.builder().email(authRequest.getUserName()).password(authRequest.getPassword()).build();
+//
+//        internalUserRepository.save(iUser);
 
         InternalUser user = internalUserRepository.findByEmail(authRequest.getUserName());
 
